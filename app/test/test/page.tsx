0025e95521514a47512e2802cadea0738271c835
michaelimmanuel <57061376+michaelimmanuel@users.noreply.github.com>
2024-12-03 19:59:31 +0700

    added contact section
    not implemented yet

--- app/test/test/page.tsx
+++ app/test/test/page.tsx
@@ -2,10 +2,11 @@ import Service from "@/components/component/test/service";
 import PeekCarousel from "@/components/component/test/prebuilt";
 import WhatSetsUsApart from "@/components/component/test/whatSetsUs";
 import PcBuilder from "@/components/component/test/pcBuilder";
+import Contact from "@/components/component/test/contact";
 export default function Page() {
     return (
         <div className="">
-            <PcBuilder/>
+            <Contact />
         </div>
     );
 }
---
+++ components/component/test/contact.tsx
@@ -0,0 +1,12 @@
+
+
+
+export default function Contact(){
+
+   
+    return (
+        <div className="flex h-dvh bg-[#141218]">
+            <iframe style={{filter: "invert(90%)"}} src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15863.72694017698!2d106.6370525!3d-6.2727072!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698d3db8c84b71%3A0x13e5cf86a48d0c6!2sRAKITINLAHID%20%7C%20Rakit%20%26%20Upgrade%20PC%20Tangerang%20Gading%20Serpong!5e0!3m2!1sen!2sid!4v1733230023354!5m2!1sen!2sid" width="400" height="300" loading="lazy" ></iframe>
+        </div>
+    )
+}
